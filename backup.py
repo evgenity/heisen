d3b50a83732b255a46a5d00ea787235a7765cb55
srsatt <srsatt@gmail.com>
2016-11-24 12:06:38 +0300

    adding blog and pretty filter labels

---
+++ backup.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import dropbox
+from dropbox.files import WriteMode
+from dropbox.exceptions import ApiError, AuthError
+
+
+# Add OAuth2 access token here.
+# You can generate one for yourself in the App Console.
+# See <https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/>
+DROPBOX_TOKEN = os.environ.get('DROPBOX_TOKEN')
+
+LOCALFILE = '/tmp/db.sqlite3'
+BACKUPPATH = '/db.sqlite3'
+
+# Uploads contents of LOCALFILE to Dropbox
+def backup():
+    with open(LOCALFILE, 'rb') as f:
+        # We use WriteMode=overwrite to make sure that the settings in the file
+        # are changed on upload
+        print("Uploading " + LOCALFILE + " to Dropbox as " + BACKUPPATH + "...")
+        try:
+            dbx.files_upload(f.read(), BACKUPPATH, mode=WriteMode('overwrite'))
+        except ApiError as err:
+            # This checks for the specific error where a user doesn't have
+            # enough Dropbox space quota to upload this file
+            if (err.error.is_path() and
+                    err.error.get_path().error.is_insufficient_space()):
+                sys.exit("ERROR: Cannot back up; insufficient space.")
+            elif err.user_message_text:
+                print(err.user_message_text)
+                sys.exit()
+            else:
+                print(err)
+                sys.exit()
+
+# Change the text string in LOCALFILE to be new_content
+# @param new_content is a string
+def change_local_file(new_content):
+    print("Changing contents of " + LOCALFILE + " on local machine...")
+    with open(LOCALFILE, 'wb') as f:
+        f.write(new_content)
+
+# Restore the local and Dropbox files to a certain revision
+def restore(rev=None):
+    # Restore the file on Dropbox to a certain revision
+    print("Restoring " + BACKUPPATH + " to revision " + rev + " on Dropbox...")
+    dbx.files_restore(BACKUPPATH, rev)
+
+    # Download the specific revision of the file at BACKUPPATH to LOCALFILE
+    print("Downloading current " + BACKUPPATH + " from Dropbox, overwriting " + LOCALFILE + "...")
+    dbx.files_download_to_file(LOCALFILE, BACKUPPATH, rev)
+
+# Look at all of the available revisions on Dropbox, and return the oldest one
+def select_revision():
+    # Get the revisions for a file (and sort by the datetime object, "server_modified")
+    print("Finding available revisions on Dropbox...")
+    revisions = sorted(dbx.files_list_revisions(BACKUPPATH, limit=30).entries,
+                       key=lambda entry: entry.server_modified)
+
+    for revision in revisions:
+        print(revision.rev, revision.server_modified)
+
+    # Return the oldest revision (first entry, because revisions was sorted oldest:newest)
+    return revisions[0].rev
+
+if __name__ == '__main__':
+    # Check for an access token
+    if (len(DROPBOX_TOKEN) == 0):
+        sys.exit("ERROR: Looks like you didn't add your access token. Open up backup-and-restore-example.py in a text editor and paste in your token in line 14.")
+    print("Creating a Dropbox object...")
+    dbx = dropbox.Dropbox(DROPBOX_TOKEN)
+    try:
+        dbx.users_get_current_account()
+    except AuthError as err:
+        sys.exit("ERROR: Invalid access token; try re-generating an access token from the app console on the web.")
+    backup()
+    print("Done!")
--- frontend/src/components/Filter.js
+++ frontend/src/components/Filter.js
@@ -7,7 +7,7 @@ export default class Filter extends React.Component{
     this.handleChange = this.handleChange.bind(this);
     }
       handleChange(event) {
-        //console.log(event.target.value, event.target.checked)
+        // console.log(event.target.value, event.target.checked)
         if (event.target.checked )
         {this.props.addTag(event.target.value);}
         else {this.props.removeTag(event.target.value)}
@@ -20,9 +20,14 @@ export default class Filter extends React.Component{
         return (
         <div className="row team-filter">
           {tags.map(function(filter, i) {
-            return (<div className='three columns filter'><input className='tag input' type="checkbox" key={i} value={filter} onChange={lambda}/> {filter} </div>)
-          })}
-        </div>
+          //   return (<div className='three columns filter'><input className='tag input' type="checkbox" key={i} value={filter} onChange={lambda}/> {filter} </div>)
+          return (<div className='three columns filter'>
+          <input className='checkbox-custom' type="checkbox" id={filter} name="cc" key={i} value={filter} onChange={lambda}/>
+          <label className='checkbox-custom-label' htmlFor={filter}>{filter}</label>
+          </div>)
+      })}
+      </div>
+
         );
       }
 
